617de8ef36205f068cfcca58ed591c0f79682dca
unknown <contact2sachin11@gmail.com>
2021-09-22 00:34:35 +0530

    first commit

---
+++ src/test/java/com/example/dynamoDB/DynamoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
